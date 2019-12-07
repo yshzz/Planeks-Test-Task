6daa90b663ea35acd8c961fe4a9cc7fc993f8d4d
yshzz <yshzz2000@gmail.com>
2019-12-07 21:38:27 +0200

    Add profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.email} profile'
