af440a33bd3524adbd5e988afe34a865e7325dfe
yshzz <yshzz2000@gmail.com>
2019-12-08 20:06:53 +0200

    Add Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from froala_editor.fields import FroalaField
 
-# Create your models here.
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        PENDING_APPROVAL = 'pending_approval'
+        PUBLISHED = 'published'
+        UNPUBLISHED = 'unpublished'
+
+    title = models.CharField(max_length=100)
+    content = FroalaField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    status = models.CharField(
+        max_length=16,
+        choices=Status.choices,
+        default=Status.PENDING_APPROVAL
+    )
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'"{self.post.title}" comment'
