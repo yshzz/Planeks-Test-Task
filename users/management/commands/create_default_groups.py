796503fcf794413b4300c089264b1842cd1adf9d
yshzz <yshzz2000@gmail.com>
2019-12-09 16:42:29 +0200

    Create manage.py command to create default permission groups

---
+++ users/management/commands/create_default_groups.py
@@ -0,0 +1,35 @@
+"""
+Create default permission groups (Users, Redactors, Admins)
+"""
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group
+from django.contrib.auth.models import Permission
+import logging
+
+
+PERMISSION_GROUPS = [
+    ('users', []),
+    ('redactors', ['publish_without_moderation']),
+    ('admins', [
+        'add_post', 'change_post',
+        'delete_post', 'view_post',
+        'add_comment', 'change_comment',
+        'delete_comment', 'view_comment',
+        'publish_without_moderation'
+    ])
+]
+
+
+class Command(BaseCommand):
+    help = 'Creates the necessary groups with the appropriate permissions'
+
+    def handle(self, *args, **options):
+        for group, permissions in PERMISSION_GROUPS:
+            new_group, created = Group.objects.get_or_create(name=group)
+            for codename in permissions:
+                try:
+                    permission = Permission.objects.get(codename=codename)
+                except Permission.DoesNotExist:
+                    logging.warning("Permission {} not found.".format(codename))
+                    continue
+                new_group.permissions.add(permission)
