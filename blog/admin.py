0110546b5753ad634d4f8579fa8bb15db31404f0
yshzz <yshzz2000@gmail.com>
2019-12-09 20:32:19 +0200

    Create admin actions for Post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from .models import Post, Comment
 
-admin.site.register(Post)
+
+def approve(modeladmin, request, queryset):
+    queryset.update(status=Post.Status.PUBLISHED)
+    approve.short_description = "Approve post"
+
+
+def decline(modeladmin, request, queryset):
+    queryset.update(status=Post.Status.UNPUBLISHED)
+    approve.short_description = "Decline post"
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    actions = [approve, decline]
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
