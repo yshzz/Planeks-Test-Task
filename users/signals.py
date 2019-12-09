2eca7da88f55b225685ee8d4006b7fcb440e2b30
yshzz <yshzz2000@gmail.com>
2019-12-09 19:16:35 +0200

    Expand signal to make every user removed from 'admins' group not a staff member

--- users/signals.py
+++ users/signals.py
@@ -29,14 +29,22 @@ def add_to_users_group(sender, instance, created, **kwargs):
         instance.groups.add(users_group)
 
 
-"""Make every user added to 'admins' group a staff member."""
+"""
+Make every user added to 'admins' group a staff member and make every user
+removed from 'admins' group not a staff member.
+"""
 @receiver(signal=m2m_changed, sender=User.groups.through)
 def manage_admins(instance, action, reverse, model, pk_set, *args, **kwargs):
-    if model == Group and action == 'post_add':
+    actions = ('post_add', 'post_remove')
+
+    if (model == Group) and (action in actions):
         try:
             admins_group_pk = Group.objects.get(name='admins').pk
         except Group.DoesNotExist:
             raise DefaultGroupNotFound('admins')
         if admins_group_pk in pk_set:
-            instance.is_staff = True
+            if action == actions[0]:
+                instance.is_staff = True
+            else:
+                instance.is_staff = False
             instance.save()
