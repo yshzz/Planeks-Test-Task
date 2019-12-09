6cee732af32eda6144a6ea0f654b20eb4a8392a5
yshzz <yshzz2000@gmail.com>
2019-12-09 18:17:15 +0200

    Register signal to add every new user to 'users' group

--- users/signals.py
+++ users/signals.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from .models import Profile
+from .exceptions import DefaultGroupNotFound
 
 User = get_user_model()
 
@@ -18,17 +19,24 @@ def save_profile(sender, instance, **kwargs):
     instance.profile.save()
 
 
-"""Make every user added to 'admins' group a staff member"""
+@receiver(post_save, sender=User)
+def add_to_users_group(sender, instance, created, **kwargs):
+    if created:
+        try:
+            users_group = Group.objects.get(name='users')
+        except Group.DoesNotExist:
+            raise DefaultGroupNotFound('users')
+    instance.groups.add(users_group)
+
+
+"""Make every user added to 'admins' group a staff member."""
 @receiver(signal=m2m_changed, sender=User.groups.through)
 def manage_admins(instance, action, reverse, model, pk_set, *args, **kwargs):
     if model == Group and action == 'post_add':
         try:
             admins_group_pk = Group.objects.get(name='admins').pk
         except Group.DoesNotExist:
-            raise ValueError(
-                'Group "admins" not found.\n'
-                'Please run "create_default_groups" command with manage.py'
-            )
+            raise DefaultGroupNotFound('admins')
         if admins_group_pk in pk_set:
             instance.is_staff = True
             instance.save()
