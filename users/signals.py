b349695ed59d5ea5d6a56245aef11620a66c6299
yshzz <yshzz2000@gmail.com>
2019-12-09 17:30:24 +0200

    Register signal to automatically make every admins group user a staff member

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,5 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
+from django.contrib.auth.models import Group
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from .models import Profile
@@ -15,3 +16,19 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+"""Make every user added to 'admins' group a staff member"""
+@receiver(signal=m2m_changed, sender=User.groups.through)
+def manage_admins(instance, action, reverse, model, pk_set, *args, **kwargs):
+    if model == Group and action == 'post_add':
+        try:
+            admins_group_pk = Group.objects.get(name='admins').pk
+        except Group.DoesNotExist:
+            raise ValueError(
+                'Group "admins" not found.\n'
+                'Please run "create_default_groups" command with manage.py'
+            )
+        if admins_group_pk in pk_set:
+            instance.is_staff = True
+            instance.save()
