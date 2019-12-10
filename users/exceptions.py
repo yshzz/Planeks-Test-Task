1be4f28e8aca05c90db61676cabeaf4a7bf581a3
yshzz <yshzz2000@gmail.com>
2019-12-10 17:38:19 +0200

    Replace old format f-strings with the new ones

--- users/exceptions.py
+++ users/exceptions.py
@@ -1,6 +1,6 @@
 class DefaultGroupNotFound(ValueError):
     def __init__(self, groupname):
-        super().__init__((
-                'Default group "{0}" not found.\n'
-                'Please run "create_default_groups" command with manage.py'
-            ).format(groupname))
+        super().__init__(
+            f'Default group "{groupname}" not found.\n'
+            'Please run "create_default_groups" command with manage.py'
+        )
