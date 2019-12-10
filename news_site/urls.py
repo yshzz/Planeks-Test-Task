c36ccd20526b8e7cbae6c70ec28bd2ae1b3a159e
yshzz <yshzz2000@gmail.com>
2019-12-10 17:36:03 +0200

    Include a blog app url config to the root url config

--- news_site/urls.py
+++ news_site/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('froala_editor/', include('froala_editor.urls')),
+    path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
