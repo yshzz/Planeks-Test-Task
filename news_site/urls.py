1f33ffbf81ada8e152728b0ace9130ebe44b791c
yshzz <yshzz2000@gmail.com>
2019-12-11 15:28:10 +0200

    Add view function to handle registration process

--- news_site/urls.py
+++ news_site/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views as user_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('froala_editor/', include('froala_editor.urls')),
+    path('register/', user_views.register, name='register'),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm, ProfileRegisterForm
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        u_form = UserRegisterForm(request.POST)
+        p_form = ProfileRegisterForm(request.POST)
+        if u_form.is_valid() and p_form.is_valid():
+            user = u_form.save()
+            user.refresh_from_db()
+
+            p_form = ProfileRegisterForm(
+                request.POST,
+                instance=user.profile
+            )
+            p_form.full_clean()
+            p_form.save()
+
+            return redirect('blog:home')
+    else:
+        u_form = UserRegisterForm()
+        p_form = ProfileRegisterForm()
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'users/register.html', context)
