848923de8c019d92664fcae3a77b750d2c989c97
yshzz <yshzz2000@gmail.com>
2019-12-11 15:29:57 +0200

    Add forms for user registration

---
+++ users/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from cuser.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from .models import Profile
+from datetime import date
+from .validators import validate_email
+
+current_year = date.today().year
+BIRTH_YEAR_CHOICES = [
+    year for year in range(current_year - 100, current_year + 1)
+]
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(
+        label=_('Email Address'),
+        max_length=254,
+        widget=forms.EmailInput(attrs={'autofocus': True}),
+        validators=[validate_email]
+    )
+    first_name = forms.CharField(
+        label=_('First Name'),
+        max_length=30,
+        required=False
+    )
+    last_name = forms.CharField(
+        label=_('Last Name'),
+        max_length=150,
+        required=False
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password1', 'password2', 'first_name', 'last_name')
+
+
+class ProfileRegisterForm(forms.ModelForm):
+    birth_date = forms.DateField(
+        widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES),
+        label=_('Birth Date'),
+        required=False
+    )
+
+    class Meta:
+        model = Profile
+        fields = ('birth_date',)
